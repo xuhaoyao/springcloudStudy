6f51c0164f54ea4d1266de0489bbf8326187a2b7
xuhaoyao <623834276@qq.com>
2021-08-19 23:06:51 +0800

    公共模块

---
+++ cloud-api-commons/src/main/java/com/scnu/springcloud/entities/Payment.java
@@ -0,0 +1,16 @@
+package com.scnu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+
+    private Long id;
+
+    private String serial;
+
+}
