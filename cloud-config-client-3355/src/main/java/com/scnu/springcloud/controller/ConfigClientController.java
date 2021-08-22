79c8e017d7e119ba599b2c49f52e6e3757f62e66
xuhaoyao <623834276@qq.com>
2021-08-23 00:10:18 +0800

    config配置中心,bus消息总线

---
+++ cloud-config-client-3355/src/main/java/com/scnu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,27 @@
+package com.scnu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    /**
+     * 测试从配置中心读取yml文件是否成功
+     *
+     */
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return "serverPort:" + serverPort + ",configInfo:" + configInfo;
+    }
+}
+ 
---
+++ cloud-config-client-3366/src/main/java/com/scnu/springcloud/ConfigClient3366Application.java
@@ -0,0 +1,11 @@
+package com.scnu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClient3366Application {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient3366Application.class,args);
+    }
+}
